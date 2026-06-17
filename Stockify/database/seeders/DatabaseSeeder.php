07a6ac87b60a9312d859df6d2ccd73f53bc0bb01
Azriel Akbar <2410817110011@mhs.ulm.ac.id>
2026-06-17 19:49:07 +0800

    feat: make an account for admin

--- Stockify/database/seeders/DatabaseSeeder.php
+++ Stockify/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::create([
+            'nama' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin123'),
+            'role' => 'admin',
         ]);
     }
 }
